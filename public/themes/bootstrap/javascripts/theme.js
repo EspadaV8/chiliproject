572d9026389d26e64430a7bd666b2056cb90b25b
Andrew Smith <espadav8@gmail.com>
2012-05-23 21:41:41 +1000

    Style tabs/forms and reposition the breadcrumb

--- public/themes/bootstrap/javascripts/theme.js
+++ public/themes/bootstrap/javascripts/theme.js
@@ -39,6 +39,10 @@
           .end();
       $('#search input').addClass('search-query span2');
 
+      $('#main').addClass('container-fluid')
+        .wrapInner('<div class="row-fluid" />');
+
+
       var breadcrumb = $('#breadcrumb');
       var bcItems = breadcrumb.html().split('»');
       var ulBreadcrumb = $('<ul class="breadcrumb" />');
@@ -48,11 +52,8 @@
       });
       ulBreadcrumb.find('li:last').addClass('active').find('.divider').remove();
 
-      breadcrumb.insertAfter(topMenu);
+      breadcrumb.prependTo($('#main'));
       breadcrumb.replaceWith(ulBreadcrumb);
-
-      $('#main').addClass('container-fluid')
-        .wrapInner('<div class="row-fluid" />');
 
       if($('#side-container').size() > 0) {
         $('#side-container').addClass('span2');
@@ -86,8 +87,57 @@
           });
       }
 
-      // $('#main-menu').addClass('row-fliud');
+      var tabs = $('.tabs');
+      if(tabs.size() > 0) {
+          tabs.find('> ul')
+                .addClass('nav nav-tabs')
+                .end()
+              .find('.tabs-buttons')
+                .remove();
+          tabs.find('a').each(function() {
+            var self = $(this);
+            var href = self.attr('href').split('/').pop();
+            if(href.indexOf('=') !== -1) {
+              href = href.split('=').pop();
+            }
+            self.attr('href', '#tab-content-' + href)
+              .attr('onclick', '')
+              .attr('data-toggle', 'tab');
+          });
+
+          $('.tab-content')
+            .removeClass('tab-content')
+            .addClass('tab-pane')
+            .attr('style', '')
+            .wrapAll('<div class="tab-content" />');
+
+          tabs.find('a:first').tab('show');
+      }
+
+      $('table').addClass('table table-condensed table-striped table-bordered');
+
+      /* forms */
+      if($('#main form').size() > 0) {
+        $('#main form').addClass('well');
+        $('#main form p').each(function() {
+            var self = $(this);
+            var label = self.find('label');
+            var text = label.text();
+            label.remove();
+
+            self.find('input,select').wrapAll('<label />');
+            if(self.find('input[type="checkbox"]').size() > 0) {
+              self.find('label').append(text);
+            } else {
+              self.find('label').prepend(text);
+            }
+        });
+        $('#main form label input[type="checkbox"]').parent().addClass('checkbox');
+        $('#main form input[type="submit"]').addClass('btn');
+      }
+
       $('.dropdown-toggle').dropdown();
+      $('#footer').appendTo('#main');
     });
   });
 })(jQuery);
